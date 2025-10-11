5163ebb9189a4a9f47fa4c192b20f6205a62937a
Kavya Reddy <kavyareddymandi21@gmail.com>
2025-10-11 15:53:25 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Contact from './components/Contact';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Portfolio">
       <Navbar />
       <Routes>
         <Route path="/" element={<Hero />} />
