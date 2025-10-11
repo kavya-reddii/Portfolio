9479ecbdb227f385ab2215d9714c6c5c4929f5b8
Kavya Reddy <kavyareddymandi21@gmail.com>
2025-10-11 15:42:04 +0530

    Initial commit for portfolio project

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Contact from './components/Contact';
 
 function App() {
   return (
-    <Router basename="/Portfolio">
+    <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<Hero />} />
